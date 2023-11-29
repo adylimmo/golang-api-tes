f9bb6a65f986b792fd7ca746523c33e11a1520fa
ady suryadi <ady.limmo@gmail.com>
2023-11-29 10:56:37 +0700

    Update main.go

--- main.go
+++ main.go
@@ -57,9 +57,9 @@ func main() {
 
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
-		message := "Welcome to Golang with Gorm and Postgres",
+		message := "Welcome to Golang with Gorm and Postgres"
 		user := "Welcome to Golang with Gorm and Postgres"
-		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message, "user"; user})
+		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message, "user": user})
 	})
 
 	AuthRouteController.AuthRoute(router)
