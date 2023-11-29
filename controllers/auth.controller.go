391b5e8e308f08baf8cabe5928fdc82b85cb8102
ady suryadi <ady.limmo@gmail.com>
2023-11-29 11:18:18 +0700

    Update auth.controller.go

--- controllers/auth.controller.go
+++ controllers/auth.controller.go
@@ -115,17 +115,8 @@ func (ac *AuthController) SignInUser(ctx *gin.Context) {
 	ctx.SetCookie("access_token", access_token, config.AccessTokenMaxAge*60, "/", "localhost", false, true)
 	ctx.SetCookie("refresh_token", refresh_token, config.RefreshTokenMaxAge*60, "/", "localhost", false, true)
 	ctx.SetCookie("logged_in", "true", config.AccessTokenMaxAge*60, "/", "localhost", false, false)
-        userResponse := &models.UserResponse{
-		ID:        newUser.ID,
-		Name:      newUser.Name,
-		Email:     newUser.Email,
-		Photo:     newUser.Photo,
-		Role:      newUser.Role,
-		Provider:  newUser.Provider,
-		CreatedAt: newUser.CreatedAt,
-		UpdatedAt: newUser.UpdatedAt,
-	}
-	ctx.JSON(http.StatusOK, gin.H{"status": "success", "access_token": access_token, "refresh_token": refresh_token, "data": gin.H{"user": userResponse} })
+      
+	ctx.JSON(http.StatusOK, gin.H{"status": "success", "access_token": access_token, "refresh_token": refresh_token })
 }
 
 // Refresh Access Token
